524b70cd6c4498ed2cb209fcba54fee3507d19f2
tomashworck <tomashworck@users.noreply.github.com>
2025-11-08 15:02:26 +0000

    Add useContract.ts, config.json, fixture-model-module.less, complex-styles-styles.py

---
+++ frontend/web/src/fragments-gateway-static/customer-template-user/complex-styles-styles.py
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/jobmatch_fhe/frontend/web/src/fragments-gateway-static/customer-template-user/complex-styles-styles.py
+// Generated at: 2025-12-03T23:45:46.543Z
+

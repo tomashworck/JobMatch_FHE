1004417adb13807fdbc5deac32bc0e21ae38688d
tomashworck <tomashworck@users.noreply.github.com>
2025-11-16 13:32:48 +0000

    Add dynamic-core-ape.jsx, download-import-common.jsx, environment-bridge-advanced.java, main-provider.js

---
+++ frontend/web/src/fragments-gateway-static/dynamic-core-ape.jsx
@@ -0,0 +1,4 @@
+export const config = { runtime: 'edge' };
+
+interface inventory_model_libProps { data: string }
+
---
+++ frontend/web/src/main-provider.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+export const environment_hooks = () => { return null; };
+
